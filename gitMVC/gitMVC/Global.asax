cb09ab9a81d91c72b448e506ad376737ca250808
sinhvien <sinhvien>
2021-04-15 08:35:48 +0700

    first commit

---
+++ gitMVC/gitMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="gitMVC.MvcApplication" Language="C#" %>
---
+++ gitMVC/gitMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(gitMVC.Startup))]
+namespace gitMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
